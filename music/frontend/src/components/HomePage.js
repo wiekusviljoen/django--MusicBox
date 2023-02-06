db35862fbfa0d49e5a74b0d7adfa4700513fa76a
wiekusviljoen <wiekusviljoen@gmail.com>
2023-02-06 20:24:47 +0200

    commit on master

--- music/frontend/src/components/HomePage.js
+++ music/frontend/src/components/HomePage.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from React-Router-Dom;
+import { Routes } from "react-router-dom";
 
 export default class HomePage extends Component {
   constructor(props) {
@@ -6,6 +10,12 @@ export default class HomePage extends Component {
   }
 
   render() {
-    return <p>Home Page</p>;
+      return (<Router>
+          <Routes>
+              <Route exact path='/'><p>Hoooome</p></Route>
+              <Route path='/join' component={RoomJoinPage} />
+              <Route path='/create' component={CreateRoomPage} />
+          </Routes>
+      </Router>);
   }
 }
